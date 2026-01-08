3f39e69e3a79677970b391f8b4d5b777d77d5307
MitehReddy <2303A51814@sru.edu.in>
2026-01-08 11:59:56 +0530

    Add files via upload

---
+++ prine_or_not.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print(False)
+else:
+    is_prime = True
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+    print(is_prime)
+# write this into opttimised code
