f9c3ae966a9f009fd536f32d128a04e9efd67a14
MitehReddy <2303A51814@sru.edu.in>
2026-01-08 12:07:22 +0530

    Optimize prime number checking logic
    Optimized the prime number checking algorithm by using isqrt and handling even numbers separately.

--- prine_or_not.py
+++ prine_or_not.py
@@ -10,3 +10,21 @@ else:
             break
     print(is_prime)
 # write this into opttimised code
+from math import isqrt
+
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print(False)
+elif n == 2:
+    print(True)
+elif n % 2 == 0:
+    print(False)
+else:
+    for i in range(3, isqrt(n) + 1, 2):
+        if n % i == 0:
+            print(False)
+            break
+    else:
+        print(True)
+
